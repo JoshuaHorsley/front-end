99e5b0a4a2e896e4d910e8ff783fd4f2ffe9c5ce
Joshorsley <joshorsley@gmail.com>
2025-06-25 17:56:46 -0400

    Integrated firebase authorization and added a login and create account page

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD89J4x6w-mjWiAOfY_4noumyBowm_NLmU",
+  authDomain: "full-stack-react-3435c.firebaseapp.com",
+  projectId: "full-stack-react-3435c",
+  storageBucket: "full-stack-react-3435c.firebasestorage.app",
+  messagingSenderId: "820905065286",
+  appId: "1:820905065286:web:5a500c332d0204b9574347"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
