c82d9c59907866e96602b044cc3bd92313ecfb88
Joshorsley <joshorsley@gmail.com>
2025-07-04 15:22:02 -0400

    started the project back up

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import useUser from "./useUser";
 
 export default function NavBar(){
-    const isLoggedIn = false;
-    const email = 'josh@gmail.com'
+   const { isLoading, user } = useUser();
 
     const navigate = useNavigate();
 
@@ -20,16 +20,22 @@ export default function NavBar(){
                 <li>
                     <Link to='/articles'>Articles</Link>
                 </li>
-                {isLoggedIn && (
+                {isLoading ? <li>Loading...</li> : (
+                    <>
+                    {user && (
                     <li style={{ color: 'white '}}>
-                        Logged in as {email}
+                        Logged in as {user.email}
                     </li>
                 )}
                 <li>
-                   {isLoggedIn 
-                   ? <button onClick={() => signOut(getAuth())}>Sign Out</button>
-                   : <button onClick={() => navigate('/login')}>Sign In</button>}
+                    {user
+                        ? <button onClick={() => signOut(getAuth())}>Sign Out</button>
+                        : <button onClick={() => navigate('/login')}>Sign In</button>
+                    }
                 </li>
+                </>
+                )}
+
             </ul>
         </nav>
     )
